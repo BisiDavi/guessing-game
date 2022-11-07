3916b36eb0b6e22026a82fbfd058bb05d0da3eb9
BisiDavi <oludavidconnect@gmail.com>
2022-11-07 01:58:37 +0100

    fibonacci updated

--- src/chapter/fibonacci.rs
+++ src/chapter/fibonacci.rs
@@ -1,44 +1,29 @@
 use std::io;
-
 fn main(){
-    fahrenheit_celsius(50.0);
-//    let result = fib(9);
-   generate_fib();
-//    println!("result: {}",result);
+    println!("Welcome to fibonacci generator, enter a 'n', and it fibonacci will be generated, enter 'exit' to exit. ");
+    loop{
+        println!("Enter 'n':");
+        let mut user_n = String::new();
+        io::stdin().read_line(&mut user_n).expect("failed to read line");
+        
+        if user_n.trim() == "exit" {
+            break;
+        }
+        
+        let user_n = match user_n.trim().parse(){
+            Ok (num) => num,
+            Err (_) => continue
+        };
+        let fib_value = fib(user_n);
+        println!("The fibonacci for the {} n series is {}:", user_n, fib_value);
+    }
 }
-
-//  Convert temperature between Fahrenheit and Celsius.
-fn fahrenheit_celsius(fahrenheit:f64) {
-    let degree = fahrenheit - 32.0;
-    let quotient = 5.0/9.0;
-    let celsius:f64 = degree * quotient;
-    println!("{}F = {:?}C",fahrenheit, celsius);
-}
-
-
 
 fn fib(n:u32) -> u32{
-    if n <= 0{
-        return 0;
-    }else if n == 1 {
+    if n <= 1 {
+        return n;
+    } else if n == 0 {
         return 1;
     }
-   fib(n-1) + fib(n-2)
-}
-
-fn generate_fib(){
-    println!("To end the program, type 'exit' ");
-    loop{
-    let mut given_n = String::new();
-    io::stdin().read_line(&mut given_n).expect("Failed to read line");
-    if given_n.trim() == "exit"{
-        break;
-    }
-    let given_n = match given_n.trim().parse(){
-        Ok(num) => num,
-        Err (_) => continue
-    };
-    let result = fib(given_n);
-    println!("result: {}",result);
-    }
+    fib(n-1) + fib(n-2)
 }
